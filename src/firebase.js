efdb8d013e23b8a2ea86971a7bd4a7c30d73cadb
Kim Juho <sv05159@naver.com>
2022-03-30 20:26:49 +0900

    0330 firebase 설정

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpvDw-nREkGpDCfiAqk4q2ZKZhLixfNWY",
+    authDomain: "nwitter-780dd.firebaseapp.com",
+    projectId: "nwitter-780dd",
+    storageBucket: "nwitter-780dd.appspot.com",
+    messagingSenderId: "30197927662",
+    appId: "1:30197927662:web:6d9f6caec7c3a18af36dff"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
