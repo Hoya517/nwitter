60c85b1ac5674ffd6250e41b50b1011136d7de9e
Kim Juho <sv05159@naver.com>
2022-03-30 21:12:21 +0900

    0330 라우터 준비하기

---
+++ src/components/Router.js
@@ -0,0 +1,13 @@
+import { HashRuter as Router, Route, Switch } from "react-router-dom";
+
+const AppRouter = () => {
+    retrurn (
+        <Router>
+            <Switch>
+                <Route />
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
